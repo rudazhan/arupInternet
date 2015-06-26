43c36ee5e6e83cdc43900edcdd44c1bbbe9755d0
ZHANG, Ruda <rudazhan@users.noreply.github.com>
2015-06-25 18:45:10 -0700

    Clean up Python syntax and add network info

--- infinitePing.py
+++ infinitePing.py
@@ -1,3 +1,13 @@
+"""Never lose internet at Arup.
+
+Throw this code into python to never lose internet (hopefully!)
+
+ARUP LAD network
+WiFi: nyquist-guest
+username: visitor.wireless
+password: 2ndavenue
+"""
+
 import urllib2
 
 x = 1
